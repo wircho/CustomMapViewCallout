1bbb722b7c5a0e885c3599214432a644dbb746df
lesyk <lesyk@users.noreply.github.com>
2016-04-20 15:14:45 +0200

    Fix: multiple callout opening

--- CustomMapView/CustomPin.swift
+++ CustomMapView/CustomPin.swift
@@ -20,35 +20,17 @@ class CustomPin: MKPinAnnotationView {
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
-        let calloutViewAdded = calloutView?.superview != nil
-        
-        
-        if (selected || !selected && hitOutside) {
+        if (selected) {
             super.setSelected(selected, animated: animated)
-        }
-        
-        self.superview?.bringSubviewToFront(self)
-        
-        
-        //Omar: The Stack Overflow answer simply writes calloutView = CalloutView
-        // here. That means it creates an empty view, which is why you don't see anything.
-        // Instead I am loading the view from the nib file Callout.xib,
-        // And I am setting a label's text using the annotation's title.
-        // Here you could also set images, buttons, etc, as long ad they are
-        // in your nib file.
-        
-        if (calloutView == nil) {
-            calloutView = NSBundle.mainBundle().loadNibNamed("Callout", owner: nil, options: nil)[0] as? CustomCallout
-            
-            calloutView?.titleLabel.text = self.annotation?.title
-        }
-        
-        if (self.selected && !calloutViewAdded) {
+            self.superview?.bringSubviewToFront(self)
+            if (calloutView == nil) {
+                calloutView = NSBundle.mainBundle().loadNibNamed("Callout", owner: nil, options: nil)[0] as? CustomCallout
+                
+                calloutView?.titleLabel.text = self.annotation?.title
+            }
             addSubview(calloutView!)
             calloutView!.center = CGPointMake(10, -calloutView!.frame.size.height / 2.0)
-        }
-        
-        if (!self.selected) {
+        } else {
             calloutView?.removeFromSuperview()
         }
     }
